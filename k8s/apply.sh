96ab7a5fafb4b5e5960b172395b2d07cb8e37848
ali-rostom1 <ali.rostom220@gmail.com>
2026-01-05 10:25:55 +0100

    config: REV-4 add redis apply commands to shell script

--- k8s/apply.sh
+++ k8s/apply.sh
@@ -1,3 +1,9 @@
+#POSTGRES
 kubectl apply -f k8s/infra/pgsql/configmap.yml
 kubectl apply -f k8s/infra/pgsql/pv.yml
 kubectl apply -f k8s/infra/pgsql/deployment.yml
+
+#REDIS
+kubectl apply -f k8s/infra/redis/pv.yml
+kubectl apply -f k8s/infra/redis/deployment.yml
+
